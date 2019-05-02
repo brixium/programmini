d47b95bf7a96f83bfa3353a8972b916539bba805
L.A <brixiumx@gmail.com>
2019-05-02 23:54:42 +0200

    CF.c now does something

--- cf/CF.c
+++ cf/CF.c
@@ -2,19 +2,46 @@
 Programma per il calcolo di CF
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+#define MAXDIM 40
 #define PADDING 'X'
+#define CFDIM 16
+
+int isVocal(char);
+char * surname(char[], int);
+
 int main(int argc, char * argv[]){
-	
-	printf("%s", );
+	char name[MAXDIM+1];
+	char sur[MAXDIM+1];
+	char sex;
+	int year;
+	int month;
+	int day;
+	char birthplace[MAXDIM+1];
+	char cf[CFDIM+1];
+
+	printf("Inserisci il tuo cognome: ");
+	scanf(" %s", &sur[0]);
+	strcpy(cf, surname(sur, strlen(sur)));
+	printf("%s\n", cf);
 	return 0;
 }
 
 char * surname(char cognome[], int dim){
-	char ris[3];
+	char * ris;
 	int i, k, flag;
+
+	ris = malloc(sizeof(char)*3);
+	if(!ris)
+		return NULL;
 	flag = 0;
 	k = 0;
+/*Converto stringa in maiuscolo*/
+	for(i=0; i<dim; i++)
+		cognome[i] = toupper(cognome[i]);
 /*Controllo consonanti*/
 	for(i=0; i<dim && k<3; i++)
 		if(!isVocal(cognome[i])){
