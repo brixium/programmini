434f7c196611fac88bbd28c41c19c7279f35b345
L.A <brixiumx@gmail.com>
2020-02-01 13:17:15 +0100

    Shell script to delete a compiled kernel version

---
+++ eliminareKernel/eliminare_kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+CURRENT_USER=$(whoami)
+ROOT=root
+KERNEL_VERSION=5.5.0+
+NOME_SCRIPT="$(basename $0)"
+
+if [ $CURRENT_USER = $ROOT ]
+then
+	rm /boot/vmlinuz-$KERNEL_VERSION
+	rm /boot/initrd-$KERNEL_VERSION
+	rm /boot/initrd.img-$KERNEL_VERSION
+	rm /boot/System-map-$KERNEL_VERSION
+	rm /boot/System.map-$KERNEL_VERSION
+	rm /boot/config-$KERNEL_VERSION
+	rm -rf /lib/modules/$KERNEL_VERSION/
+	rm /var/lib/initramfs/$KERNEL_VERSION
+	rm /var/lib/initramfs-tools/$KERNEL_VERSION
+	update-grub2
+else
+	echo "Per eseguire questo script sono richiesti i privilegi di root. Esegui:"
+	echo "sudo sh $NOME_SCRIPT"
+fi
