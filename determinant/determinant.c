4170404e774e21c695d88385de99fa9cbd0cfa31
Lorenzo <brixiumx@gmail.com>
2020-03-24 19:17:49 +0100

    Added Sarrus rule for 4x4, which emproves performance for huge matrixes

--- determinant/determinant.c
+++ determinant/determinant.c
@@ -3,11 +3,13 @@
 #include <time.h>
 
 #define DEBUG 0
-#define STOPWATCH 1
-#define SARRUS 1
+#define STOPWATCH 0
 #define LEIBNIZ 1
+#define SARRUS 1
+#define SARRUS4X4 1
 
 long det(int *, int); /*returns the determinant*/
+void swapColumns(int *, int, int, int); /*input: address of matrix, number of columns, index of first column, i of second column*/
 
 int main(int argc, char * argv[]){
 	int ncol, nelements, *values, i;
@@ -63,13 +65,44 @@ long det(int * matrix, int ncol){
 		sa1 = 0;
 		sa2 = 0;
 		for(i=0; i<ncol; i++){
-			sa1 = sa1 + *(matrix + (0*ncol) + i) * *( matrix + (1*ncol) + ((i+1)%3) ) * *( matrix + (2*ncol) + ((i+2)%3) ) ;
-			sa2 = sa2 + *(matrix + (2*ncol) + i) * *( matrix + (1*ncol) + ((i+1)%3) ) * *( matrix + (0*ncol) + ((i+2)%3) ) ;
+			sa1 = sa1 + *(matrix + (0*ncol) + i) * *( matrix + (1*ncol) + ((i+1)%ncol) ) * *( matrix + (2*ncol) + ((i+2)%ncol) ) ;
+			sa2 = sa2 + *(matrix + (2*ncol) + i) * *( matrix + (1*ncol) + ((i+1)%ncol) ) * *( matrix + (0*ncol) + ((i+2)%ncol) ) ;
 		}
 		#if DEBUG
 		printf("\nsa1:%ld, sa2:%ld\n", sa1, sa2);
 		#endif
 		return sa1-sa2;
+	}
+	#endif
+	#if SARRUS4X4
+	#if DEBUG
+	printf("\n");
+	#endif
+	partial = 0;
+	if(ncol == 4){
+		for(j=0; j < 3; j++){
+			for(i=0; i<ncol; i++){
+				if( (j == 0 && i%2 == 0) || (j>0 && i%2 == 1) )
+					minus = 1;
+				else
+					minus = -1;
+				partial += *(matrix + (0*ncol) + i) * *( matrix + (1*ncol) + ((i+1)%ncol) ) * *( matrix + (2*ncol) + ((i+2)%ncol) ) * *( matrix + (3*ncol) + ((i+3)%ncol) ) * minus;
+				partial += *(matrix + (3*ncol) + i) * *(matrix + (2*ncol) + ((i+1)%ncol) ) * *( matrix + (1*ncol) + ((i+2)%ncol) ) * *( matrix + (0*ncol) + ((i+3)%ncol) ) * minus;
+				#if DEBUG
+				printf("(i:%d,j:%d) I'm doing: %d * %d * %d * %d * %d(=minus)\n", i, j, *(matrix+i), *(matrix+ncol+((i+1)%4)), *(matrix+2*ncol+(i+2)%4), *(matrix+3*ncol+(i+3)%4), minus);
+				printf("(i:%d,j:%d) I'm doing: %d * %d * %d * %d * %d(=minus)\n", i, j, *(matrix+(3*ncol)+i), *(matrix+(2*ncol)+((i+1)%4)), *( matrix + ncol + ( (i+2) %4 ) ), *(matrix+((i+3)%4)), minus);
+				#endif
+			}
+			if(j==0)
+				swapColumns(matrix, 4, 1, 2);
+			else if(j == 1){
+				swapColumns(matrix, 4, 1, 2);
+				swapColumns(matrix, 4, 2, 3);
+			}else if(j == 2){
+				swapColumns(matrix, 4, 2, 3);
+			}
+		}
+		return partial;
 	}
 	#endif
 	/*Laplace*/
@@ -112,3 +145,15 @@ long det(int * matrix, int ncol){
 	}
 }
 
+void swapColumns(int * m, int ncol, int ic1, int ic2){
+	int i, aux;
+	#if DEBUG
+	printf("Changing column %d with column %d\n", ic1, ic2);
+	#endif
+	for(i=0; i<ncol; i++){
+		aux = *(m+(i*ncol)+ic1);
+		*(m+(i*ncol)+(ic1)) = *(m+(i*ncol)+(ic2));
+		*(m+(i*ncol)+(ic2)) = aux;
+	}
+	return;
+}
